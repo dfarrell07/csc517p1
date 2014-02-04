c787f56c30bf07ad4b186dd2a36e9ed106182509
Daniel Farrell <dfarrell07@gmail.com>
2014-02-04 14:53:38 -0500

    Basic rails app auto-created

---
+++ backchannel/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backchannel::Application.config.secret_key_base = '26126602bf40ce833ba4dfeaafef9a672e681800930c112317a4cdf8d1dffe3d7f6af3ba094c6fc0cf4f29779c5b6b5ce50c95a8eda540d1146530a569a7d0ce'
