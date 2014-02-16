b2d5b16d1fbbf5c597a964466658612a918e5f35
Daniel Farrell <dfarrell07@gmail.com>
2014-02-15 20:13:53 -0500

    Proper access control for posts, added useful links

--- backchannel/app/controllers/posts_controller.rb
+++ backchannel/app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
   before_filter :get_post, only: [:show, :update, :edit, :destroy]
-  before_filter :check_auth, :only => [:edit]
+  before_filter :check_owns, :only => [:edit, :update]
+  before_filter :check_owns_or_admin, :only => [:destroy]
 
   def create
     @post = Post.new(post_params)
-    #@post.user_id = session[:user_id]
-    @post.user_id = 1234 
+    @post.user_id = session[:user_id]
+    #@post.user_id = 1234 
     @post.save
     redirect_to @post
   end
 
   def show
+    @user_name = User.find(@post.user_id).user_name
   end
 
   def index
@@ -45,10 +47,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def check_auth
+  def check_owns
     if session[:user_id] != @post.user_id
-      flash[:notice] = "You can only edit your own posts"
-      redirect_to @post
+      flash[:notice] = "You can only edit your own posts!"
+      redirect_to posts_path
+    end
+  end
+
+  def check_owns_or_admin
+    if session[:user_id] != @post.user_id and User.find(session[:user_id]).rights == "user"
+      flash[:notice] = "You can only edit your own posts!"
+      redirect_to posts_path
     end
   end
 
